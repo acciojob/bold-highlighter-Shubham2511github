947dc378730b5391bbedf5c97f98424b3a413966
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-23 00:54:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+
 function highlight() {
     //Write your code here
-
+	
+    var boldWords = document.querySelectorAll("p strong");
+    for (var i = 0; i < boldWords.length; i++) {
+        boldWords[i].style.color = "green";
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
+	var boldWords = document.querySelectorAll("p strong");
+	for(let i =0; i< boldWords.length; i++){
+		boldWords[i].style.color = "black";
+	}
 
     
 }
